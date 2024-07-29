555968fff35746714a21209d445b966f278decae
trxyz.gg <96659201+trxyzgg@users.noreply.github.com>
2024-07-29 02:29:16 +0200

    Fast r15 animations.lua

---
+++ Fast r15 animations.lua
@@ -0,0 +1,13 @@
+wait = task.wait
+local a = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:wait()
+
+while wait() do
+    local b = a:FindFirstChildOfClass("Humanoid") or a:FindFirstChildOfClass("AnimationController")
+    if not b or not a then continue end
+    for c, d in next, b:GetPlayingAnimationTracks() do
+        d:AdjustSpeed(15)
+    end
+end
+
+-- Optional
+game.Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').WalkSpeed = 1
